6a6dc1cbd62f7e45c43e18bd0cd7db7d94d70221
Bruno Almeida <brunokfsop@gmail.com>
2020-06-25 18:57:03 -0300

    Changed date types from ConfigDate Model

--- src/models/ConfigDate.js
+++ src/models/ConfigDate.js
@@ -3,10 +3,10 @@ const { Model, DataTypes } = require('sequelize')
 class ConfigDate extends Model {
   static init (sequelize) {
     super.init({
-      init_create_date: DataTypes.DATEONLY,
-      end_create_date: DataTypes.DATEONLY,
-      init_subscription_date: DataTypes.DATEONLY,
-      end_subscription_date: DataTypes.DATEONLY,
+      init_create_date: DataTypes.DATE,
+      end_create_date: DataTypes.DATE,
+      init_subscription_date: DataTypes.DATE,
+      end_subscription_date: DataTypes.DATE,
       status: DataTypes.STRING(1)
     }, {
       sequelize
